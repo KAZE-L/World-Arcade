815887765be9c6e0f53b30c431b96d6cdfb07458
GINTOAHC <chaotingchen10@gmail.com>
2025-06-10 23:02:57 +0800

    fix: use embed

--- src/app/AmericanSuperheroFlight/page.js
+++ src/app/AmericanSuperheroFlight/page.js
@@ -1,6 +1,16 @@
-'use client';
-import IntroScreen from './components/IntroScreen';   // ← 退兩層，找到根目錄的 components
+'use client'
+import Link from 'next/link'
 
-export default function HomePage() {
-  return <IntroScreen />;
+export default function ShadowRun() {
+  return (
+    <div className="h-screen w-full flex flex-col">
+      <Link className="bg-gray-400" href="/">
+        Back
+      </Link>
+      <iframe
+        src="https://superhero-game.vercel.app"
+        className="grow w-full"
+      ></iframe>
+    </div>
+  )
 }
