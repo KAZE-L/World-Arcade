f1208723b40ab107ba7b57d9b84e108ba87c77b5
GINTOAHC <chaotingchen10@gmail.com>
2025-06-11 04:03:58 +0800

    feat: bubble tea game

--- src/app/BubbleTeaPearlGame/page.js
+++ src/app/BubbleTeaPearlGame/page.js
@@ -1,7 +1,15 @@
+import Link from 'next/link'
+
 export default function BubbleTeaPearlGame() {
   return (
-    <div>
-      <h1>Bubble Tea Pearl Game</h1>
+    <div className="h-screen w-full flex flex-col">
+      <Link className="bg-gray-400" href="/">
+        Back
+      </Link>
+      <iframe
+        src="https://bubble-tea.vercel.app/"
+        className="grow w-full"
+      ></iframe>
     </div>
-  );
+  )
 }
